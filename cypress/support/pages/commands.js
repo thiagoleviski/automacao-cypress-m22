3fcdee24c731c30143414b0a4d3af2c4555492c7
Thiago Leviski <thiago_leviski@yahoo.com.br>
2023-03-10 12:49:40 -0300

    Third

--- cypress/support/pages/commands.js
+++ cypress/support/pages/commands.js
@@ -4,7 +4,6 @@ Cypress.Commands.add('logout', () => {
 
     cy.get(`#main > div > nav > ul > li.woocommerce-MyAccount-navigation-link.woocommerce-MyAccount-navigation-link--customer-logout > a`).then(($href)=>{
         const wpnonce = $href.attr('href').split("/?")[1]
-        cy.log(wpnonce)
         cy.request({
             url: `/minha-conta/customer-logout/?${wpnonce}`,
             method: "GET"
